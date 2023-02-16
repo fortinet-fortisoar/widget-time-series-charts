7bd7346e474e4fc473157af0c3f2a934fd7737eb
kerestan <bkerestan@fortinet.com>
2023-02-16 15:50:12 -0500

    Initial commit

---
+++ widget/view.controller.js
@@ -0,0 +1,43 @@
+'use strict';
+(function () {
+    angular
+        .module('cybersponse')
+        .controller('timeSeriesCharts100Ctrl', timeSeriesCharts100Ctrl);
+
+        timeSeriesCharts100Ctrl.$inject = ['$scope', '$timeout', '$resource', 'config'];
+
+    function timeSeriesCharts100Ctrl($scope, $timeout, $resource, config) {
+        $scope.processing=true;
+        $scope.config = config;
+        $scope.init = init;
+        $scope.errMsg = "No results for this chart yet. Data generation playbook has been triggred and results will populate shortly.";
+
+        function init() {
+            var dataFormat = {
+                bindto: "#c3Chart-"+config.correlationValue
+            };
+
+            $resource('api/3/advanced_charts/:uuid').get({uuid:$scope.config.record_uuid}).$promise.then(function(data) {  
+                if (!data.queryResults) {
+                    $scope.noData=true;
+                    $scope.processing=false;
+                }
+                else {
+                    angular.forEach(data.queryResults, function(value, key) {
+                        dataFormat[key] = value;
+                    })
+
+                    $timeout(function() {
+                        c3.generate(dataFormat);
+                        $scope.noData=false;
+                        $scope.processing=false;
+                        },
+                    0,
+                    false)
+                }
+            })
+        }
+        
+        init();
+    }
+})();
